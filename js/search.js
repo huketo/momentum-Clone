09ae8e6d298e63eb7132596e654efb3fdbb9cabe
huketo <75968351+huketo@users.noreply.github.com>
2021-09-05 21:19:41 +0900

    Search Engine Select

--- js/search.js
+++ js/search.js
@@ -1,2 +1,43 @@
-const searchCover = document.querySelector(".search-cover");
+const searchBar = document.querySelector("div .search-bar");
+const searchForm = document.querySelector(".search-form");
 const searchUnderbar = document.querySelector(".search-underbar");
+const searchEngine = document.querySelector(".search-engine");
+const searchEngineImg = document.querySelector(".search-engine__img");
+const searchEngineCheck = document.querySelector("#search-engine__check");
+const searchEngineSelect = document.querySelector(".fa-chevron-down");
+const searchInput = document.querySelector("#search-keyword");
+
+const FOCUSING_KEY = "focusing";
+
+searchInput.style.cursor = "pointer";
+searchEngineCheck.checked = "true";
+
+searchBar.addEventListener("mouseover", () => {
+  if (searchInput.style.cursor === "pointer")
+    searchUnderbar.classList.add(FOCUSING_KEY);
+});
+
+searchBar.addEventListener("mouseleave", () => {
+  if (searchInput.style.cursor === "pointer")
+    searchUnderbar.classList.remove(FOCUSING_KEY);
+});
+
+searchInput.addEventListener("focus", () => {
+  searchUnderbar.classList.add(FOCUSING_KEY);
+  searchEngineImg.classList.add(FOCUSING_KEY);
+  searchInput.style.cursor = "text";
+});
+
+searchInput.addEventListener("blur", () => {
+  searchUnderbar.classList.remove(FOCUSING_KEY);
+  searchEngineImg.classList.remove(FOCUSING_KEY);
+  searchInput.style.cursor = "pointer";
+});
+
+searchEngineSelect.addEventListener("click", () => {
+  if (searchEngineCheck.checked) {
+    searchEngineSelect.classList.add(FOCUSING_KEY);
+  } else {
+    searchEngineSelect.classList.remove(FOCUSING_KEY);
+  }
+});
