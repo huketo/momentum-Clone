e060590f29cbdeba783ceecf4b42a340260c210e
huketo <75968351+huketo@users.noreply.github.com>
2021-09-06 05:41:59 +0900

    http -> https

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "a4f0a56cbb643c2908bb425ee10d5d7a";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
@@ -11,7 +11,7 @@ function onGeoOk(position) {
       const weather = document.querySelector("#weather .weather-temp");
       const city = document.querySelector("#weather .weather-city");
       weatherIcon.classList.remove(HIDDEN_CLASSNAME);
-      weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       weather.innerText = `${data.main.temp} ℃`;
       city.innerText = data.name;
     });
